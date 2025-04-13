1dcf4cf56dad615f4259c6882503005f57831821
Nnomaddd <tobylashley@gmail.com>
2025-04-13 05:15:30 -0400

    hopefully final

--- ex_09_1.py
+++ ex_09_1.py
@@ -1,6 +1,10 @@
-fhand = open('romeo.txt')
-for line in fhand:
-    line = line.rstrip()
-    line = line.split()
-    
-    print(line)
+with open('romeo.txt') as fhand:
+    all_words = []
+    for line in fhand:
+        line = line.lower()
+        line = line.rstrip()
+        words = line.split()
+        all_words.extend(words)
+    all_words.sort()
+    print(all_words)
+
